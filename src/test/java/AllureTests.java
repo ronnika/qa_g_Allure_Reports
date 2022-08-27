6b67b7957b7ca8920258e875edf6e6c8831c845a
ronnika <lunkestr@gmail.com>
2022-08-27 15:49:22 +0300

    refactor

--- src/test/java/AllureTests.java
+++ src/test/java/AllureTests.java
@@ -1,11 +1,7 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
